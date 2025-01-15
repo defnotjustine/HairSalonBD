8d9e340eb48af6960989afe04f9aff65a5ca126a
Weronika Gut <weragut05@gmail.com>
2025-01-15 13:46:13 +0100

    Commit 1. Added Hairdresser class, remember to check the password to database twice

--- src/main/java/com/pwr/model/DatabaseConnection.java
+++ src/main/java/com/pwr/model/DatabaseConnection.java
@@ -4,18 +4,18 @@ import java.sql.*;
 public class DatabaseConnection {
     private static Connection connection;
 
-    public DatabaseConnection() {
+    public DatabaseConnection() throws SQLException {
         try {
             this.connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/hair_salon", "root", "root");
+                    "jdbc:mysql://localhost:3306/hair_salon", "root", "haslo");
         } catch (SQLException e) {
             e.printStackTrace();
-            //throw new SQLException("Nie udało się połączyć z bazą danych");
+            throw new SQLException("Nie udało się połączyć z bazą danych");
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/hair_salon", "root", "root");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/hair_salon", "root", "haslo");
     }
 
     public void close() {
